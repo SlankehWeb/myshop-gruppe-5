4a1c1c3ab706cb98aa13b2c350dc08e1ab18aa20
Rasmus Sondergaard <rasmusmadsen6@hotmail.com>
2023-06-29 16:47:57 +0200

    Slide update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@
 
 import { ChakraProvider } from '@chakra-ui/react';
 import Products from './components/products';
+import Slide from './components/highlights';
+
 
 function App() {
   return (
     <ChakraProvider>
       <div className="App">
+        <Slide />
         <Products />
       </div>
     </ChakraProvider>
